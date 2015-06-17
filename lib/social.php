afec6e0f131ac270b669e5f84c1f6d90bea83d98
Wayne Moulden <waynemou@localhost.localdomain>
2015-06-18 09:36:34 +1000

    Fixes typos, implemented social tags, analytics and link to rulebook from WotC

---
+++ lib/social.php
@@ -0,0 +1,38 @@
+  <!-- properties -->
+  <meta name="viewport" content="width-device-width,initial-scale=1"/>
+  <meta name="robots" content="index, nofollow">
+  <link rel="canonical" href="http://msheroes.mameau.com/" />
+
+  <!--social/analytics tags -->
+  <meta http-equiv="Content-type" content="text/html;charset=utf-8" />
+  <meta name="description" content="An unofficial browser based implementation of Monster Slayers - The Heroes of Hesiod from Wizards of the Coast"/>
+  <meta name="keywords" content="wizard, dungeon, dragon, wotc, coast, dnd, tabletop, roll20, rogue, thief, rpg, roleplay, dice">
+  <meta name="author" content="sairuk"/>
+
+
+  <!-- Google Authorship and Publisher Markup -->
+  <link rel="author" href="https://plus.google.com/113462917042894028038/posts"/>
+  <link rel="publisher" href="https://plus.google.com/113462917042894028038"/>
+
+
+  <!-- Schema.org markup for Google+ -->
+  <meta itemprop="name" content="Wizards of the Coast">
+  <meta itemprop="description" content="An unofficial browser based implementation of Monster Slayers - The Heroes of Hesiod from Wizards of the Coast.">
+  <meta itemprop="image" content="http://msheroes.mameau.com/img/logo.png"> 
+
+  <!-- Twitter Card data -->
+  <meta name="twitter:card" content="summary">
+  <meta name="twitter:site" content="@sairukau">
+  <meta name="twitter:title" content="Wizards of the Coast">
+  <meta name="twitter:description" content="An unofficial browser based implementation of Monster Slayers - The Heroes of Hesiod from Wizards of the Coast.">
+  <meta name="twitter:creator" content="@sairukau">
+  <!-- Twitter Summary card images must be at least 200x200px -->
+  <meta name="twitter:image" content="http://msheroes.mameau.com/img/logo.png"> 
+
+  <!-- Open Graph data -->
+  <meta property="og:title" content="Wizards of the Coast" />
+  <meta property="og:description" content="An unofficial browser based implementation of Monster Slayers - The Heroes of Hesiod from Wizards of the Coast." /> 
+  <meta property="og:type" content="website" />
+  <meta property="og:url" content="http://msheroes.mameau.com" />
+  <meta property="og:image" content="http://msheroes.mameau.com/img/logo.png" />
+  <!-- end social -->
